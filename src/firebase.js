218267d7f4586d87421fe835bb726ec710e41fef
pauago1-design <pauago1@gmail.com>
2026-06-16 13:48:24 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAj4Ld6eHovDWfFmGqckUfNhTaOC8MtxyE",
-  authDomain: "indices-ventilatorios.firebaseapp.com",
-  projectId: "indices-ventilatorios",
-  storageBucket: "indices-ventilatorios.firebasestorage.app",
-  messagingSenderId: "613087672619",
-  appId: "1:613087672619:web:ce9237db81d173e7342d6e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
